c884fa88e55dc53767113c0f7796a36259c46e04
Jason Umali <jasonu18@tamu.edu>
2021-03-25 16:29:37 -0500

    changed some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   root :to => "books#index"
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/new'
-  # get 'books/edit'
-  # get 'books/delete'
+   get 'books/index'
+   get 'books/show'
+   get 'books/new'
+   get 'books/edit'
+   get 'books/delete'
 
   #
-  resources :books do
+  resources :book do
     member do
       get :delete
+      get :show
+      post :create
+      post :edit
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
